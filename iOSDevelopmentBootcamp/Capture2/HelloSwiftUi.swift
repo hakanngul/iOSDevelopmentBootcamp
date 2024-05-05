a11cf400d49ced834e2eefd6afc7c5cbf9088b84
Hakan <hakanngul@icloud.com>
2024-05-06 00:20:12 +0300

    Section 2: SwiftUI Fundamentals: Getting Started

--- iOSDevelopmentBootcamp/Capture2/HelloSwiftUi.swift
+++ iOSDevelopmentBootcamp/Capture2/HelloSwiftUi.swift
@@ -8,16 +8,41 @@
 import SwiftUI
 
 struct HelloSwiftUi: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        }.padding()
+var myImage = "https://www.worldatlas.com/r/w1200/upload/d6/1f/60/shutterstock-1051182467.jpg"
+  var body: some View {
+    VStack {
+      Image("costa-rica")
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .clipShape(RoundedRectangle(cornerRadius: 25, style: .continuous));
+        
+        AsyncImage(url: URL(string: myImage)) { image in
+            image.resizable()
+                .aspectRatio(contentMode: .fit)
+                .clipShape(RoundedRectangle(cornerRadius: 25, style: .continuous))
+        } placeholder: {
+            //Text("Downloading...")
+            ProgressView("Downloading...")
+        }
+
+            
+      Text("First Line")
+        .foregroundStyle(.cyan)
+        .font(.title3)
+        .padding([.top, .bottom], 20)
+      Text("Second Line")
+        .foregroundStyle(.green)
+      Text("Third Line")
+      HStack {
+        Text("Left")
+        Text("Right").fontWeight(.heavy)
+      }
     }
+    .foregroundStyle(.orange)
+    .padding()
+  }
 }
 
-#Preview {
-    HelloSwiftUi()
+#Preview{
+  HelloSwiftUi()
 }
